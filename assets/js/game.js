bd6fb7bc545faedf178f932ac435e834697243b6
svnday <40030806+svnday@users.noreply.github.com>
2021-11-15 23:36:08 -0600

    additional js testing

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,8 @@
-window.alert("This is an alert! Javascript is running!");
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
