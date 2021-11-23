0abc8a14443925240b70a9028cb77b9f13b87d80
svnday <40030806+svnday@users.noreply.github.com>
2021-11-23 15:54:55 -0600

    removed debugger

--- assets/js/game.js
+++ assets/js/game.js
@@ -95,8 +95,7 @@ var startGame = function() {
     for(var i = 0; i < enemyInfo.length; i++) {
         if (playerInfo.health > 0) {
             window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ) );
-            debugger;
-
+        
             var pickedEnemyObj = enemyInfo[i];;
             pickedEnemyObj.health = randomNumber(40, 60);
             // debugger;
